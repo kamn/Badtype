80a7eb05c5c41cf4c91589847fb6ed7d9774a4ed
kamn <kamn@users.noreply.github.com>
2015-03-17 21:53:13 -0600

    Started to make it actually useful

---
+++ badtype.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+var esprima = require('esprima');
+var badtype = require('./src/parser')
+
+//TODO: Take a list of files or folder
+
+var file = process.argv[2];
+
+var filepath = path.join(__dirname, file);
+
+var data = fs.readFileSync(filepath);
+
+var parsed = badtype.typeParse(data);
+
+badtype.typeCheckParse(parsed, [])
